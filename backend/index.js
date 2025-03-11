e1fde422239942f261d8b755bb954043aeb1cccf
RTYROCKERS <7143.ruchir@gmail.com>
2025-03-10 21:55:52 -1200

    added the send message functionality sing socket.io

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,12 @@ import dotenv from 'dotenv';
 // dotenv.config({
 //   path : "./.env"
 // }); //
-
+import http, { get } from 'http';
 dotenv.config();
 import express from 'express';
-
-
+import {Server} from 'socket.io';
+import Message from './models/Message.model.js';
+import Group from './models/Group.model.js';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import connectDB from './config/mongodb.config.js';
@@ -16,7 +17,47 @@ import userRoutes from "./routes/user.routes.js";
 import groupRoutes from './routes/group.routes.js'
 import { singleAvatar } from './middlewares/multer.middleware.js';
 import seedUsers from './seeders/user.seeder.js';
+import TryCatch from './utils/TryCatch.util.js';
 const app = express();
+const server1=http.createServer(app);
+const io=new Server(server1,{
+  cors:{
+    origin:"*",
+    methods:["GET","POST"]
+  }
+});
+io.on("connection",(socket)=>{
+  console.log("user conected");
+  socket.on("joinGroup",(groupId)=>{
+    socket.join(groupId);
+    console.log(`User joined group: ${groupId}`);
+  });
+  socket.on("sendMessage",TryCatch(async (groupId,senderId,content,attachments)=>{
+    
+      const group=await Group.findById(groupId);
+      if (!group){
+        return socket.emit("Group not found");
+      }
+      const newMessage = new Message({
+        sender: senderId,
+        group: groupId,
+        content: content,
+        attachments:attachments
+    });
+      await newMessage.save();
+      io.to(groupId).emit("receiveMessage",{
+        _id: newMessage._id,
+        sender: senderId,
+        group: groupId,
+        content: content,
+        attachments:attachments,
+        //can send timestamp to.....
+      });
+  }));
+  socket.on("disconnect",()=>{
+    console.log(`${socket.id} has disconnected.....`);
+  })
+});
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
