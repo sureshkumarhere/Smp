29b482de06e19f5aaf6c3d20451381daf6636cfe
Suresh Kumar <sureshkumar79061@gmail.com>
2025-04-12 09:58:07 +0530

    Removed some files

--- backend/routes/Post.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-
-const router = express.Router();
-
-import {createPost} from "../controllers/post.controller.js"
-router.post('/createpost', createPost);
-
-
-export default router;
--- frontend/src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Home = ({isLoggedIn}) => {
-  return (
-    <div className="h-full text-white text-3xl flex justify-center items-center">Home</div>
-  )
-}
-
-export default Home
